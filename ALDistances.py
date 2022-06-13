5f5bf50c24b183678a1ec71ed0529eea79e6a832
CSITSCAM <88196352+CSITSCAM@users.noreply.github.com>
2022-06-13 11:52:45 +0530

    Create ALDistances.py

---
+++ ALDistances.py
@@ -0,0 +1,4 @@
+from scipy.spatial import distance
+print("Euclidean Distance: ", distance.euclidean([0,3,4,5],[7,6,3,-1])
+print("Minkowski Distance: ", distance.minkowski([0,3,4,5],[7,6,3,-1],3)
+print("Jaccard Distance: ", distance.jaccard([1,1,1,1],[0,1,0,0])
