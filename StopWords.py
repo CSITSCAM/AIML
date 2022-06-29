78f2ac18ed3c753996dcddeeadb1b2cf29da2d0b
CSITSCAM <88196352+CSITSCAM@users.noreply.github.com>
2022-06-29 10:45:59 +0530

    Update StopWords.py

--- StopWords.py
+++ StopWords.py
@@ -1,22 +1,20 @@
-#!pip install nltk
+!pip install nltk
+text=['This is introduction to NLP','It is likely to be useful,to people ','Machine learning is the new electrcity','There would be less hype around AI and more action going forward','python is the best tool!','R is good langauage','I like this book','I want more books like this']
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-  
-example_sent = """This is a sample sentence,
-                  showing off the stop words filtration."""
-  
-stop_words = set(stopwords.words('english'))
-  
-word_tokens = word_tokenize(example_sent)
-  
-filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
-  
-filtered_sentence = []
-  
-for w in word_tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
-  
-print(word_tokens)
-print(filtered_sentence)
+import pandas as pd
+
+convert list to data frame
+df = pd.DataFrame({'tweet':text})
+
+print(df)
+
+!pip install nltk import nltk nltk.download() from nltk.corpus import stopwords
+
+import nltk nltk.download('stopwords')
+
+stop = stopwords.words('english')
+
+df['tweet'] = df['tweet'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+
+df['tweet']
+
