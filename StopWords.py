a6c286c00c3ce2ac5894b3f56f6e7d2c66d64dac
CSITSCAM <88196352+CSITSCAM@users.noreply.github.com>
2022-06-29 10:47:21 +0530

    Update StopWords.py

--- StopWords.py
+++ StopWords.py
@@ -1,16 +1,16 @@
 !pip install nltk
+import nltk 
+import pandas as pd
+nltk.download()
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
 text=['This is introduction to NLP','It is likely to be useful,to people ','Machine learning is the new electrcity','There would be less hype around AI and more action going forward','python is the best tool!','R is good langauage','I like this book','I want more books like this']
 
-import pandas as pd
-
-convert list to data frame
+#convert list to data frame
 df = pd.DataFrame({'tweet':text})
 
 print(df)
-
-!pip install nltk import nltk nltk.download() from nltk.corpus import stopwords
-
-import nltk nltk.download('stopwords')
 
 stop = stopwords.words('english')
 
